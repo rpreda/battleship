422ca88bb0be5770a341c38a3287fa97eccc00e7
rpreda <rares.preda98@gmail.com>
2015-05-27 21:42:48 +0300

    Starting GUI for client

---
+++ Client_GUI/Main_form.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_GUI
+{
+    public partial class Main_form : Form
+    {
+        public static string ip;
+        public static string username;
+        public Main_form()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Server_dialog connection_info = new Server_dialog();
+            connection_info.Show();
+            this.button1.Hide();
+        }
+
+        private void debug_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
